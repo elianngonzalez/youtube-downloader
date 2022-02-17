60ed14a44916395a90bd3b25b68976d173539483
elian gonzalez <elianngonzalez7@gmail.com>
2022-02-17 02:44:16 -0300

    preueva4

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 SECRET_KEY = 'bihb4swnw7d4fwec8datlmjbn8rklk9bed0v8g1xnbnbpug7yt3xprvwe6vhqjec34sy0nvoz774ynnb87y85vaw7hxzip1y1016c9gpd5axaf3fegsmruwpy3hei8sve4q4kvw3f8vr4ok691lukv648sur0zgnkh5yw67bugrr73ie0qu4tw0t7uhuome6j8jugtsu4rxxuc39ni6f64jmwrnq6u9g93pmg1vshcya96fwph73s5ocur'
 
-path = os.getcwd() + '\\output\\'
+path = os.getcwd() + '/output/'
 
 
 @app.route('/')
